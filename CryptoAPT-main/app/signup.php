3f2c9364a141d9cd241d7275c4ab7173ddf32fe0
SantiQuintas <104693899+SantiQuintas@users.noreply.github.com>
2022-10-26 20:33:25 -0300

    Add files via upload

--- CryptoAPT-main/app/signup.php
+++ CryptoAPT-main/app/signup.php
@@ -8,57 +8,68 @@
 </head>
 <body>
 <?php
-      
-    
+
+
       $conexion = mysqli_connect("localhost","root","","cripto");
-      
+
      if($_POST['cpass']==$_POST['pass'])
      {
-       $cnmail= "SELECT * from user where email= '" . $_POST['email'] ."'" ;
-       $consul = mysqli_query($conexion, $cnmail);
-       $datos = mysqli_fetch_array($consul);
-    
-       if($datos['email']== $_POST['email'] || $datos['user']==$_POST['user'])
-      {
-        echo '<script type="text/javascript">'; 
-        echo 'alert("USUARIO YA REGISTRADO");'; 
-        echo 'window.location.href = "signup.html";';
-        echo '</script>';
-        //<script>alert("Usuario ya registrado") window.location.href = "</script>!-->
-       
-        exit();
-      }
+         if(strlen($_POST['pass'])>=8)
+         {
+
+
+               $cnmail= "SELECT * from user where email= '" . $_POST['email'] ."'" ;
+               $consul = mysqli_query($conexion, $cnmail);
+               $datos = mysqli_fetch_array($consul);
+
+                 if($datos['email']== $_POST['email'] || $datos['user']==$_POST['user'])
+                {
+                  echo '<script type="text/javascript">';
+                  echo 'alert("USUARIO YA REGISTRADO");';
+                  echo 'window.location.href = "signup.html";';
+                  echo '</script>';
+                  //<script>alert("Usuario ya registrado") window.location.href = "</script>!-->
+
+                  exit();
+                }
+                else
+                {
+                $cn="INSERT INTO user (user,email,pass) values ('".$_POST['user']."','" .$_POST['email']."','" .$_POST['pass']."')";
+                $consultas = mysqli_query($conexion, $cn);
+
+                echo '<script type="text/javascript">';
+                echo 'alert("REGISTRADO CORRECTAMENTE");';
+                echo 'window.location.href = "login.html";';
+                echo '</script>';
+              }
+          }
+
         else
         {
-        $cn="INSERT INTO user (user,email,pass) values ('".$_POST['user']."','" .$_POST['email']."','" .$_POST['pass']."')";    
-        $consultas = mysqli_query($conexion, $cn);
-
-        echo '<script type="text/javascript">'; 
-        echo 'alert("REGISTRADO CORRECTAMENTE");'; 
-        echo 'window.location.href = "login.html";';
-        echo '</script>';  
-      
-      
-          
+          echo '<script type="text/javascript">';
+          echo 'alert("LA CONTRASEÑA DEBE CONTENER MAS DE 8 CARACTERES");';
+          echo 'window.location.href = "signup.html";';
+          echo '</script>';
         }
-     }
+      }
+
      else
      {
-      echo("logeo fallido");
-      header("location: login.html");
-      exit();
+       echo '<script type="text/javascript">';
+       echo 'alert("SU CONTRASEÑA NO COINCIDE CON AL CONFIRMACION");';
+       echo 'window.location.href = "signup.html";';
+       echo '</script>';
      }
-  
-      
-      
-      
-          
-      
-      
-      
-  
-      
+
+
+
+
+
+
+
+
+
+
   ?>
 </body>
 </html>
- 
