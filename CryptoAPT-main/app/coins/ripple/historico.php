d27ab7019a41c43d7e418182d8e83efc164b6054
SantiQuintas <104693899+SantiQuintas@users.noreply.github.com>
2022-11-03 09:10:32 -0300

    Add files via upload

---
+++ CryptoAPT-main/app/coins/ripple/historico.php
@@ -0,0 +1,23 @@
+<?php
+
+$mon=$_REQUEST['btc'];
+echo($mon);
+
+//BASE DE DATOS
+$conexion = mysqli_connect("localhost","root","","cripto");
+$a=0;
+while($a==0)
+{
+    $cn="INSERT INTO ripple(precio) values ('$mon')";
+    $consulta=mysqli_query($conexion,$cn);
+    sleep(1800);
+    header('location: carga.html');
+    exit();
+
+}
+
+?>
+
+ 
+ 
+
