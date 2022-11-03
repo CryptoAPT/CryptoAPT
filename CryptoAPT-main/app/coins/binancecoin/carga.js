770fe29f9707063c63c4ed2a01df8faa5f11bae0
SantiQuintas <104693899+SantiQuintas@users.noreply.github.com>
2022-11-03 09:04:58 -0300

    Add files via upload

---
+++ CryptoAPT-main/app/coins/binancecoin/carga.js
@@ -0,0 +1,8 @@
+//binancecoin=>USD
+fetch("https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=usd")
+    .then(response=> response.json()) 
+    .then(data=> window.location="historico.php?btc=" + data.binancecoin.usd)
+    
+
+
+    
